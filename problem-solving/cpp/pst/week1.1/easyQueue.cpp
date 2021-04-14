f14890cd0c93e0e3b2a50120bcbee63f1629c211
Ahmad Ateya <ahmad.m.ateya@gmail.com>
2021-04-14 03:01:12 +0200

    week1.1 problems

---
+++ problem-solving/cpp/pst/week1.1/easyQueue.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// explained by Sama Yasser
+// reference https://ideone.com/zDyFTj
+
+#define endl "\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() {
+
+    // to make the cin & cout fast
+    IOS
+
+}
---
+++ problem-solving/cpp/pst/week1.1/ferryLoadingVI.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// explained by Emad Tarek
+// reference https://ideone.com/nr5f0V
+#define endl "\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() {
+
+    // to make the cin & cout fast
+    IOS
+
+}
