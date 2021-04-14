e6b9377c732f81951c54d5015718b9522ec387d3
Ahmad Ateya <ahmad.m.ateya@gmail.com>
2021-04-14 03:22:10 +0200

    week2.1 problems

---
+++ problem-solving/cpp/pst/week2.1/physicsPractical.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// explained by
+// Omar Hany https://ideone.com/b4pZAs
+#define endl "\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() {
+    // to make the cin & cout fast
+    IOS
+
+}
---
+++ problem-solving/cpp/pst/week2.1/twoArraysAndSwaps.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// explained by
+// Omar Hany https://ideone.com/m7Phir
+
+#define endl "\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() {
+    // to make the cin & cout fast
+    IOS
+
+}
