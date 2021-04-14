c53bfd0ae9d4fa92bd76701a37e2c38a2c27718d
Ahmad Ateya <ahmad.m.ateya@gmail.com>
2021-04-14 03:14:44 +0200

    week1.2 problems

---
+++ problem-solving/cpp/pst/week1.2/chatOrder.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// explained by
+// Omar Hany https://ideone.com/KX8fFO
+
+#define endl "\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() {
+
+    // to make the cin & cout fast
+    IOS
+
+}
---
+++ problem-solving/cpp/pst/week1.2/errorCorrection.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// explained by
+// Emad Tarek https://ideone.com/3i9rqd
+// Omar Hany https://ideone.com/9UMcOg
+#define endl "\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() {
+
+    // to make the cin & cout fast
+    IOS
+
+}
