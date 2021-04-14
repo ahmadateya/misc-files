fb7ce1df66f293b018229d6836700cbb604e7b12
Ahmad Ateya <ahmad.m.ateya@gmail.com>
2021-04-14 17:40:51 +0200

    easyQueue - done

--- problem-solving/cpp/pst/week1.1/easyQueue.cpp
+++ problem-solving/cpp/pst/week1.1/easyQueue.cpp
@@ -2,15 +2,33 @@
 
 using namespace std;
 
-// explained by Sama Yasser
-// reference https://ideone.com/zDyFTj
-
 #define endl "\n"
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int t;
+queue<int> q;
 
 int main() {
 
     // to make the cin & cout fast
     IOS
-
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if(x == 1){ // operation 1
+            int n;
+            cin>>n;
+            q.push(n); // enqueue the element
+        }
+        else if(x == 2) {  // operation 2
+            // dequeue the last element or do nothing
+            // ONLY dequeue WITHOUT printing it
+            if(!q.empty()) q.pop();
+        }
+        else { // operation 3
+            if(!q.empty()) cout<<q.front()<<endl;
+            else cout<<"Empty!"<<endl;
+        }
+    }
+    return 0;
 }
