768b6395571e8b2946f26fdd9a79fc630853e7a5
Ahmad Ateya <ahmad.m.ateya@gmail.com>
2021-04-15 02:11:26 +0200

    physicsPractical - done

--- problem-solving/cpp/pst/week2.1/physicsPractical.cpp
+++ problem-solving/cpp/pst/week2.1/physicsPractical.cpp
@@ -2,13 +2,34 @@
 
 using namespace std;
 
-// explained by
-// Omar Hany https://ideone.com/b4pZAs
-#define endl "\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+const int N = 1e5 + 5;
+int n;
+int a[N];
 
-int main() {
-    // to make the cin & cout fast
-    IOS
+int main()
+{
+    freopen("input.txt","r", stdin);
+    freopen("output.txt","w",stdout);
 
+    cin>>n;
+    for(int i = 0; i < n; i++) cin>>a[i];
+
+    sort(a , a + n);
+
+    int cnt = 0;
+    int mn = n;
+
+    // 1- we walked through the array and assumed that its the minimum element
+    for(int i = 0; i < n; i++) {
+        // 2- we get the first element bigger than the current element *2 (a[i] * 2)
+        int idx = upper_bound(a , a + n, a[i] * 2) - a;
+        // 3- we calculate the elements that is before the current element (i)
+        // and the elements that is after the current element (n - idx)
+        cnt = i + n - idx; // cnt = number of elements that will be deleted (number of operations)
+        // 4- we store the minimum value between the number of operations we did every time
+        mn = min(mn , cnt);
+    }
+    cout<<mn;
+
+    return 0;
 }
