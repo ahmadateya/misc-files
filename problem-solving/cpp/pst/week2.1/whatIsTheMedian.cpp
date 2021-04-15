e973d682f92e7bac22efb4d61be01627b8d68300
Ahmad Ateya <ahmad.m.ateya@gmail.com>
2021-04-15 02:58:05 +0200

    whatIsTheMedian - done

--- problem-solving/cpp/pst/week2.1/whatIsTheMedian.cpp
+++ problem-solving/cpp/pst/week2.1/whatIsTheMedian.cpp
@@ -2,15 +2,26 @@
 
 using namespace std;
 
-// explained by
-// Omar Hany https://ideone.com/Y6etQ0
-
-#define endl "\n"
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define sz(v)         ((int)((v).size()))
+#define endl         "\n"
+#define IOS          ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 int main() {
-
-    // to make the cin & cout fast
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
     IOS
-
+    int n;
+    vector<int> v;
+    while (cin >> n) {
+        v.push_back(n);
+        sort(v.begin(), v.end());
+        int sz = v.size();
+        if (sz % 2 == 0) {
+            // this v[sz / 2]  returns int because of the line 5
+            cout << (v[sz / 2] + v[sz / 2 - 1]) / 2 << endl;
+        } else
+            cout << v[sz / 2] << endl;
+    }
 }
